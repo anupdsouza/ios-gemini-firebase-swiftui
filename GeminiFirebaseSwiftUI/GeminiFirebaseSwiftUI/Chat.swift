02cd5b4df3d265558c16e81ef72b83e7e49a568d
Anup DSouza <anup.dsouza@gmail.com>
2024-05-02 09:16:58 +0530

    Update chat model & add chatservice file

--- GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/Chat.swift
+++ GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/Chat.swift
@@ -10,3 +10,47 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct ChatDocument: Codable {
+    let createTime: Timestamp
+    let prompt: String
+    let response: String?
+    let status: Status
+    
+    struct Status: Codable {
+        let startTime: Timestamp?
+        let completeTime: Timestamp?
+        let updateTime: Timestamp
+        let state: String
+        let error: String?
+        
+        var chatState: ChatState {
+            return ChatState(rawValue: state) ?? .PROCESSING
+        }
+    }
+}
+
+enum ChatState: String, Codable {
+    case COMPLETED
+    case ERROR
+    case PROCESSING
+}
+
+struct Chat: Hashable {
+    private(set) var id: UUID = .init()
+    var text: String?
+    var isUser: Bool
+    var state: ChatState = .PROCESSING
+    var message: String {
+        switch state {
+        case .COMPLETED:
+            return text ?? ""
+        case .ERROR:
+            return "Something went wrong. Please try again."
+        case .PROCESSING:
+            return "..."
+        }
+    }
+}
