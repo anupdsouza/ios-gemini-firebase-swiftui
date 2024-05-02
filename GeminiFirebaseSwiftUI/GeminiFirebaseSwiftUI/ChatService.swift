91f572c90827b8a7944a8f6c7eac8a8e196db712
Anup DSouza <anup.dsouza@gmail.com>
2024-05-02 10:01:50 +0530

    Update chat service & ui

--- GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/ChatService.swift
+++ GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/ChatService.swift
@@ -10,3 +10,42 @@
 //
 
 import Foundation
+import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+@Observable class ChatService {
+    private(set) var messages: [Chat] = []
+    private var db = Firestore.firestore()
+    private let collectionPath = "generate"
+    
+    func fetchMessages() {
+        db.collection(collectionPath)
+            .order(by: "createTime", descending: false)
+            .addSnapshotListener { [weak self] querySnapshot, error in
+                guard let self else { return }
+                guard let documents = querySnapshot?.documents else {
+                    print("No documents found")
+                    return
+                }
+                
+                messages = documents.compactMap { snapshot -> [Chat]? in
+                    do {
+                        let document = try snapshot.data(as: ChatDocument.self)
+                        let prompt = Chat(text: document.prompt, isUser: true, state: .COMPLETED)
+                        let response = Chat(text: document.response ?? "", isUser: false, state: document.status.chatState)
+                        return [prompt, response]
+                    } catch {
+                        print(error.localizedDescription)
+                        return nil
+                    }
+                }.flatMap { $0 }
+            }
+    }
+    
+    func sendMessage(_ message: String) {
+        let placeholderMessages = [Chat(text: message, isUser: true, state: .COMPLETED), Chat(text: "", isUser: false)]
+        messages.append(contentsOf: placeholderMessages)
+        db.collection(collectionPath).addDocument(data: ["prompt": message])
+    }
+}
--- GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/ContentView.swift
+++ GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/ContentView.swift
@@ -12,14 +12,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textInput = ""
+    @State private var chatService = ChatService()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            titleView()
+            chatListView()
+            inputView()
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .foregroundStyle(.white)
+        .background {
+            Color.black.ignoresSafeArea()
+        }
+        .onAppear(perform: {
+            // MARK: Fetch messages
+            chatService.fetchMessages()
+        })
+    }
+    
+    // MARK: Title view
+    @ViewBuilder private func titleView() -> some View {
+        VStack(alignment: .center, content: {
+            HStack(alignment: .center, content: {
+                Image("gemini")
+                    .resizable()
+                    .scaledToFit()
+                Image(systemName: "plus")
+                    .resizable()
+                    .scaledToFit()
+                    .bold()
+                    .frame(height: 20)
+                Image("firebase")
+                    .resizable()
+                    .scaledToFit()
+            })
+            .frame(height: 50)
+        })
+    }
+    
+    // MARK: Chat list view
+    @ViewBuilder private func chatListView() -> some View {
+        ScrollViewReader(content: { proxy in
+            ScrollView {
+                ForEach(chatService.messages, id: \.self) { chatMessage in
+                    chatMessageView(chatMessage)
+                        .id(chatMessage.id)
+                }
+            }
+            .onChange(of: chatService.messages) { oldValue, newValue in
+                guard let recentMessage = chatService.messages.last else { return }
+                DispatchQueue.main.async {
+                    withAnimation {
+                        proxy.scrollTo(recentMessage.id, anchor: .bottom)
+                    }
+                }
+            }
+        })
+    }
+    
+    // MARK: Input view
+    @ViewBuilder private func inputView() -> some View {
+        HStack {
+            TextField("Enter a message...", text: $textInput)
+                .textFieldStyle(.roundedBorder)
+                .foregroundStyle(.black)
+            Button(action: sendMessage, label: {
+                Image(systemName: "paperplane.fill")
+            })
+        }
+    }
+    
+    // MARK: Chat message view
+    @ViewBuilder private func chatMessageView(_ chat: Chat) -> some View {
+        ChatBubble(direction: chat.isUser ? .right : .left) {
+            Text(chat.message)
+                .font(.title3)
+                .padding(.all, 20)
+                .foregroundStyle(.white)
+                .background {
+                    chat.isUser ? Color.blue : Color.green
+                }
+        }
+    }
+    
+    // MARK: Send message
+    private func sendMessage() {
+        chatService.sendMessage(textInput)
+        textInput = ""
     }
 }
 
