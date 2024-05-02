30bfbc35ba4ec9564a3fec0612ead85e10f713d4
Anup DSouza <anup.dsouza@gmail.com>
2024-05-02 09:09:46 +0530

    Add chat shape helpers & chat model file

---
+++ GeminiFirebaseSwiftUI/GeminiFirebaseSwiftUI/Chat.swift
@@ -0,0 +1,12 @@
+//
+//  Chat.swift
+//  GeminiFirebaseSwiftUI
+//
+//  Created by Anup D'Souza on 02/05/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
+//
+
+import Foundation
